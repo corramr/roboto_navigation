03530cd11be3979d12336fb8c852700178689efe
corra <s292706@studenti.polito.it>
2026-02-14 12:45:47 +0100

    restoring turtlebot example

--- app/nav2_ws/src/navigation2/nav2_bringup/launch/tb3_simulation_launch.py
+++ app/nav2_ws/src/navigation2/nav2_bringup/launch/tb3_simulation_launch.py
@@ -91,7 +91,7 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
+        default_value='true',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
@@ -240,9 +240,9 @@ def generate_launch_description():
     ld.add_action(declare_use_respawn_cmd)
 
     # # Add any conditioned actions
-    # ld.add_action(start_gazebo_server_cmd)
-    # ld.add_action(start_gazebo_client_cmd)
-    # ld.add_action(start_gazebo_spawner_cmd)
+    ld.add_action(start_gazebo_server_cmd)
+    ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(start_gazebo_spawner_cmd)
 
     # Add the actions to launch all of the navigation nodes
     ld.add_action(start_robot_state_publisher_cmd)
