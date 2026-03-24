d1c96f8b069c9628b9f7ee652be3c19fd80c51b9
corra <s292706@studenti.polito.it>
2026-03-24 19:55:49 +0100

    adjusting range and height of /scan

--- app/nav2_ws/src/pointcloud_to_laserscan/launch/livox_launch.py
+++ app/nav2_ws/src/pointcloud_to_laserscan/launch/livox_launch.py
@@ -21,12 +21,12 @@ def generate_launch_description():
                 'target_frame': 'base_scan',
                 'transform_tolerance': 0.01,
                 'min_height': 0.1,
-                'max_height': 0.7,
+                'max_height': 0.45,
                 'angle_min': -3.14159,    # -180 degrees
                 'angle_max': 3.14159,     # +180 degrees
                 'angle_increment': 0.004, #0.0087,  # 0.5 degrees
                 'scan_time': 0.1,  # NOT IMPO: just metadata info
-                'range_min': 0.1,
+                'range_min': 0.32,
                 'range_max': 10.0,
                 'inf_epsilon': 1.0,
                 'use_inf': True
